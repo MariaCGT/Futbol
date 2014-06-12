1c4092f8aff63414ac89f08d7bc158a3611e978c
MariaCGT <maria@debian>
2014-06-12 10:57:45 +0200

    prueba

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,24 +1,61 @@
-from bottle import route, default_app, run
-from funciones import cuadrado
+#coding:utf-8
 
-@route('/name/<name>')
-def nameindex(name='Stranger'):
-    return '<strong>Hola, %s!</strong>' % name
- 
-@route('/')
-def index():
-    return '<strong>Hello %d</strong>' % cuadrado(3)
-
-# This must be added in order to do correct path lookups for the views
+import requests
+import json
 import os
-from bottle import TEMPLATE_PATH
+from bottle import request, get, post, run, debug, route, template,error, TEMPLATE_PATH
+import bottle
 
+#Pagina principal de la app, con las distintas opciones de búsqueda
+
+@route('/')
+def buscar():
+	return template('index')
+
+@get('/bbva')
+def bbva():
+    return template('bbva')
+    
+@get('/segunda')
+def segunda():
+	return template('adelante')
+    
+@route('/clasificacion1')
+def clasificacion1():
+	dicc_parametros = {'key':'94c694751928db22f60b189594f8c5b6','format':'json','league':'1','req':'tables'}
+	r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=dicc_parametros)
+	datos = json.loads(r.text.encode("utf-8"))
+	return template('clasificacion1',datos=datos)
+	
+@route('/jornadas1')
+def jornadas1():
+	dicc_parametros = {'key':'94c694751928db22f60b189594f8c5b6','format':'json','league':'1','req':'matchs','round':'12'}
+	r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=dicc_parametros)
+	datos = json.loads(r.text.encode("utf-8"))
+	return template('jornada',datos=datos)
+	
+@route('/partidos1')
+def partidos1():
+	dicc_parametros = {'key':'94c694751928db22f60b189594f8c5b6','format':'json','req':'matchsday','date':'2012-08-19','top':'1','limit':'1'}
+	r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=dicc_parametros)
+	datos = json.loads(r.text.encode("utf-8"))
+	return template('partido',datos=datos)
+	
+@route('/clasificacion2')
+def clasificacion1():
+	dicc_parametros = {'key':'94c694751928db22f60b189594f8c5b6','format':'json','league':'2','req':'tables'}
+	r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=dicc_parametros)
+	datos = json.loads(r.text.encode("utf-8"))
+	return template('clasificacion2',datos=datos)
+	
+	
+	
 ON_OPENSHIFT = False
 if os.environ.has_key('OPENSHIFT_REPO_DIR'):
     ON_OPENSHIFT = True
 
 if ON_OPENSHIFT:
-    TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'], 
+    TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'],
                                       'runtime/repo/wsgi/views/'))
     
     application=default_app()
